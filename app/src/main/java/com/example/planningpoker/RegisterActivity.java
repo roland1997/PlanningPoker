29b0cfd029d554cf989acb8dc9dcb1030c6173e1
roland1997 <45368747+roland1997@users.noreply.github.com>
2019-10-24 16:15:45 +0300

    roland

--- app/src/main/java/com/example/planningpoker/RegisterActivity.java
+++ app/src/main/java/com/example/planningpoker/RegisterActivity.java
@@ -3,12 +3,21 @@ package com.example.planningpoker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText name,email,password;
+    Button regiszter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        name = (EditText)findViewById(R.id.nameLabel);
+        email = (EditText)findViewById(R.id.emailLabel);
+        password = (EditText)findViewById(R.id.passwordLabel);
+        regiszter = (Button)findViewById(R.id.registerButton);
     }
 }
