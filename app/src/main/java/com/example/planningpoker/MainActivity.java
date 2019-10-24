9f4151b53bc0e85134076f360246e15a184d2c8e
roland1997 <45368747+roland1997@users.noreply.github.com>
2019-10-24 17:00:08 +0300

    roland2

--- app/src/main/java/com/example/planningpoker/MainActivity.java
+++ app/src/main/java/com/example/planningpoker/MainActivity.java
@@ -2,15 +2,46 @@ package com.example.planningpoker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText lName, lPassword;
+    Button lLogin, lRegiszter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lName = (EditText)findViewById(R.id.lNameLabel);
+        lPassword = (EditText)findViewById(R.id.lPasswordLabel);
+        lLogin = (Button)findViewById(R.id.lLoginButton);
+        lRegiszter = (Button)findViewById(R.id.lRegisterButton);
 
-        int i=5;
+        lRegiszter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityRegister();
+            }
+        });
+        lLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitySession();
+            }
+        });
+
+    }
+    public void openActivityRegister(){
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+    }
+    public void openActivitySession(){
+        Intent intent = new Intent(this,SessionActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/planningpoker/RegisterActivity.java
+++ app/src/main/java/com/example/planningpoker/RegisterActivity.java
@@ -2,22 +2,36 @@ package com.example.planningpoker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText name,email,password;
-    Button regiszter;
+    EditText rName,rEmail, rPassword;
+    Button rRegiszter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        name = (EditText)findViewById(R.id.nameLabel);
-        email = (EditText)findViewById(R.id.emailLabel);
-        password = (EditText)findViewById(R.id.passwordLabel);
-        regiszter = (Button)findViewById(R.id.registerButton);
+        rName = (EditText)findViewById(R.id.rNameLabel);
+        rEmail = (EditText)findViewById(R.id.rEmailLabel);
+        rPassword = (EditText)findViewById(R.id.rPasswordLabel);
+        rRegiszter = (Button)findViewById(R.id.rRegisterButton);
+
+        rRegiszter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            openActivityMain();
+            }
+        });
+    }
+
+    public void openActivityMain(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
